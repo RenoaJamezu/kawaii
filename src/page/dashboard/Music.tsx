9e73bbb1bd361d796f826398468f85c0e572fd2d
Kurasao <jamerolenor@gmail.com>
2025-12-25 23:38:34 +0800

    fix: update heading structure in Music component for better clarity

--- src/page/dashboard/Music.tsx
+++ src/page/dashboard/Music.tsx
@@ -28,8 +28,9 @@ export default function Music() {
   return (
     <div className="bg-white rounded-xl w-full h-full p-5 overflow-y-auto fade-slide-top custom-scrollbar">
       <h1 className="font-display text-primary font-bold text-xl mb-3">
-        Songs for you to listen
+        Songs for you to listen: <br />
       </h1>
+      <h1 className="font-display text-primary font-bold text-xl mb-3 text-center">{currentTrack?.title || "None"}</h1>
 
       <Accordion
         type="single"
