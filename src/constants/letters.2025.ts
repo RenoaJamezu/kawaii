84ab7cb1647974cc8b1561b95b21d362c5e193b8
Kurasao <jamerolenor@gmail.com>
2025-12-29 21:12:16 +0800

    added message and gif

--- src/constants/letters.2025.ts
+++ src/constants/letters.2025.ts
@@ -16,4 +16,12 @@ export const LETTERS_2025: LoveLetterMeta[] = [
     envKey: "VITE_LETTER_2025_12_28",
     gif: "/gifs/peachy-celebrate.gif",
   },
+  {
+    id: "2025-12-31",
+    title: "New Year's Message",
+    date: "Dec 31, 2025",
+    unlockAt: new Date("2025-12-31T23:59:59").getTime(),
+    envKey: "VITE_LETTER_2025_12_31",
+    gif: "/gifs/jumping-peach.gif",
+  },
 ];
